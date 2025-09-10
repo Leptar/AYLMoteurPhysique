4b6998f9104f84700972e09a032b34d46e949cb6
Léo Cheikh-Boukal <leo.ultimate34@gmail.com>
2025-09-10 15:06:16 -0400

    Add 3DVector class basics
    Add .h and .cpp for 3DVector.
    Will add more function (distance, angle, projection...)

---
+++ MoteurPhysique/src/3DVector.cpp
@@ -0,0 +1,59 @@
+//
+// Created by leoul on 2025-09-09.
+//
+
+#include "3DVector.h"
+
+#include <cmath>
+
+Vector3D::Vector3D() {
+    x = 0;
+    y = 0;
+    z = 0;
+}
+
+Vector3D::Vector3D(float x, float y, float z) {
+    this->x = x;
+    this->y = y;
+    this->z = z;
+}
+
+Vector3D Vector3D::operator*(const float &v) const {
+    return Vector3D(x * v, y * v, z * v);
+}
+
+Vector3D Vector3D::operator+(const Vector3D &v) const {
+    return Vector3D(x + v.x, y + v.y, z + v.z);
+}
+
+Vector3D Vector3D::operator-(const Vector3D &v) const {
+    return Vector3D(x - v.x, y - v.y, z - v.z);
+}
+
+Vector3D Vector3D::normalize() {
+    float norm = GetNorm();
+    x = x / norm;
+    y = y / norm;
+    z = z / norm;
+    return *this;
+}
+
+float Vector3D::dot(const Vector3D &v) const {
+    return x * v.x + y * v.y + z * v.z;
+}
+
+Vector3D Vector3D::cross(const Vector3D &v) const {
+    float x1 = y * v.z - z * v.y;
+    float y1 = z * v.x - x * v.z;
+    float z = x * v.y - y * v.x;
+
+    return Vector3D(x1, y1, z);
+}
+
+float Vector3D::GetNorm() {
+    return sqrtf(x * x + y * y + z * z);
+}
+
+float Vector3D::GetSquareNorm() {
+    return x * x + y * y + z * z;
+}
