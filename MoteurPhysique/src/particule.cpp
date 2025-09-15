774681c22d247e3d9981f1d62097a44c06ec4d55
ThoX <antonyhuynh2003@gmail.com>
2025-09-15 04:13:44 +0200

    Nouvelle classe Particule avec Vector3D

---
+++ MoteurPhysique/src/particule.cpp
@@ -0,0 +1,52 @@
+#include "Particule.h"
+
+Particule::Particule(double px, double py, double pz,
+                     double vx, double vy, double vz,
+                     double ax, double ay, double az,
+                     double masse)
+    : _x(px), _y(py), _z(pz),
+      _vx(vx), _vy(vy), _vz(vz),
+      _ax(ax), _ay(ay), _az(az) 
+{
+    setMasse(masse);
+}
+
+double Particule::getX() const { return _x; }
+double Particule::getY() const { return _y; }
+double Particule::getZ() const { return _z; }
+
+double Particule::getVx() const { return _vx; }
+double Particule::getVy() const { return _vy; }
+double Particule::getVz() const { return _vz; }
+
+double Particule::getAx() const { return _ax; }
+double Particule::getAy() const { return _ay; }
+double Particule::getAz() const { return _az; }
+
+double Particule::getInverseMasse() const { return _inverseMasse; }
+
+void Particule::setPosition(double px, double py, double pz) {
+    _x = px;
+    _y = py;
+    _z = pz;
+}
+
+void Particule::setVitesse(double vx, double vy, double vz) {
+    _vx = vx;
+    _vy = vy;
+    _vz = vz;
+}
+
+void Particule::setAcceleration(double ax, double ay, double az) {
+    _ax = ax;
+    _ay = ay;
+    _az = az;
+}
+
+void Particule::setMasse(double masse) {
+    if (masse <= 0.0) {
+        _inverseMasse = 0.0;
+    } else {
+        _inverseMasse = 1.0 / masse;
+    }
+}
---
+++ MoteurPhysique/src/particule.h
@@ -0,0 +1,49 @@
+#ifndef PARTICULE_H
+#define PARTICULE_H
+
+class Particule {
+private:
+    double _x;
+    double _y;
+    double _z;
+
+    double _vx;
+    double _vy;
+    double _vz;
+
+    double _ax;
+    double _ay;
+    double _az;
+
+    double _inverseMasse;
+
+public:
+    Particule(double px = 0.0, double py = 0.0, double pz = 0.0,
+              double vx = 0.0, double vy = 0.0, double vz = 0.0,
+              double ax = 0.0, double ay = 0.0, double az = 0.0,
+              double masse = 1.0);
+
+    double getX() const;
+    double getY() const;
+    double getZ() const;
+
+    double getVx() const;
+    double getVy() const;
+    double getVz() const;
+
+    double getAx() const;
+    double getAy() const;
+    double getAz() const;
+
+    double getInverseMasse() const;
+
+    void setPosition(double px, double py, double pz);
+
+    void setVitesse(double vx, double vy, double vz);
+
+    void setAcceleration(double ax, double ay, double az);
+
+    void setMasse(double masse);
+};
+
+#endif 
