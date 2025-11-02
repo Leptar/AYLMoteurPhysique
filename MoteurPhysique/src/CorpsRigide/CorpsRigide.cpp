e8be80b21c4278b70148b3c08a8db04ccd6d104b
ThoX <antonyhuynh2003@gmail.com>
2025-11-02 03:10:33 -0500

    Debut de corps rigide

---
+++ MoteurPhysique/src/CorpsRigide/CorpsRigide.cpp
@@ -0,0 +1,121 @@
+#include "CorpsRigide.h"
+#include <cmath> 
+
+// --- Constructeur ---
+CorpsRigide::CorpsRigide() :
+    m_position(0, 0, 0),
+    m_velocite(0, 0, 0),
+    m_inverseMasse(1.0f),
+    m_orientation(1, 0, 0, 0),
+    m_velociteAngulaire(0, 0, 0),
+    m_linearDamping(0.99f),
+    m_angularDamping(0.99f),
+    m_forceAccum(0, 0, 0),
+    m_torqueAccum(0, 0, 0)
+{
+    setInverseInertiaTensorBody(Matrix3::identity());
+    
+    _updateTransformMatrix();
+    _updateInverseInertiaTensorWorld();
+}
+
+void CorpsRigide::setPosition(const Vector3D& pos)
+{
+    m_position = pos;
+    _updateTransformMatrix();
+}
+
+void CorpsRigide::setVelocite(const Vector3D& vel)
+{
+    m_velocite = vel;
+}
+
+void CorpsRigide::setMasse(float masse)
+{
+    if (masse <= 0.0f) {
+        m_inverseMasse = 0.0f;
+    } else {
+        m_inverseMasse = 1.0f / masse;
+    }
+}
+
+void CorpsRigide::setOrientation(const Quaternion& q)
+{
+    m_orientation = q;
+    
+    _updateTransformMatrix();
+    _updateInverseInertiaTensorWorld();
+}
+
+void CorpsRigide::setVelociteAngulaire(const Vector3D& velAng)
+{
+    m_velociteAngulaire = velAng;
+}
+
+void CorpsRigide::setInverseInertiaTensorBody(const Matrix3& tensor)
+{
+    m_inverseInertiaTensorBody = tensor;
+    
+    _updateInverseInertiaTensorWorld();
+}
+
+void CorpsRigide::setLinearDamping(float damping)
+{
+    m_linearDamping = damping;
+}
+
+void CorpsRigide::setAngularDamping(float damping)
+{
+    m_angularDamping = damping;
+}
+
+
+Vector3D CorpsRigide::getPosition()
+{
+    return m_position;
+}
+
+Quaternion CorpsRigide::getOrientation()
+{
+    return m_orientation;
+}
+
+const Matrix4& CorpsRigide::getTransformMatrix()
+{
+    return m_transformMatrix;
+}
+
+void CorpsRigide::addForce(const Vector3D& force)
+{
+    // À FAIRE
+}
+
+void CorpsRigide::addForceAtPoint(const Vector3D& force, const Vector3D& worldPoint)
+{
+    // À FAIRE
+}
+
+void CorpsRigide::addForceAtBodyPoint(const Vector3D& force, const Vector3D& bodyPoint)
+{
+    // À FAIRE
+}
+
+void CorpsRigide::clearAccumulators()
+{
+    // À FAIRE
+}
+
+void CorpsRigide::_updateTransformMatrix()
+{
+    // À FAIRE
+}
+
+void CorpsRigide::_updateInverseInertiaTensorWorld()
+{
+    // À FAIRE
+}
+
+void CorpsRigide::integrer(float deltaTime)
+{
+    // À FAIRE
+}
---
+++ MoteurPhysique/src/CorpsRigide/CorpsRigide.h
@@ -0,0 +1,59 @@
+#pragma once
+
+#include "MathStruct/3DVector.h"
+#include "MathStruct/Quaternion.h"
+#include "MathStruct/Matrix3.h"
+#include "MathStruct/Matrix4.h"
+
+class CorpsRigide
+{
+private:
+    // Etat
+    Vector3D m_position;
+    Vector3D m_velocite;
+    float    m_inverseMasse;
+
+    Quaternion m_orientation;
+    Vector3D   m_velociteAngulaire;
+
+    // Propriétés physiques
+    Matrix3  m_inverseInertiaTensorBody;
+    float m_linearDamping;
+    float m_angularDamping;
+
+    // Forces
+    Vector3D m_forceAccum;
+    Vector3D m_torqueAccum;
+
+    // Jeu de tir ballistique
+    Matrix4 m_transformMatrix;
+    Matrix3 m_inverseInertiaTensorWorld;
+
+    void _updateTransformMatrix();
+    void _updateInverseInertiaTensorWorld();
+
+public:
+    // Setters
+    void setPosition(const Vector3D& pos);
+    void setVelocite(const Vector3D& vel);
+    void setMasse(float masse);
+    void setOrientation(const Quaternion& q);
+    void setVelociteAngulaire(const Vector3D& velAng);
+
+    void setInverseInertiaTensorBody(const Matrix3& tensor);
+    void setLinearDamping(float damping);
+    void setAngularDamping(float damping);
+
+    // Getters
+    Vector3D getPosition();
+    Quaternion getOrientation();
+    const Matrix4& getTransformMatrix();
+
+    // Gestion des forces et torques
+    void addForce(const Vector3D& force);
+    void addForceAtPoint(const Vector3D& force, const Vector3D& worldPoint);
+    void addForceAtBodyPoint(const Vector3D& force, const Vector3D& bodyPoint);
+    void clearAccumulators();
+
+    void integrer(float deltaTime);
+};
